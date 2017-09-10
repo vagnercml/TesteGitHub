22a792717a12ca169f3977e6b4b0fe1c54fa23c6
vagnercml <vagnercml@hotmail.com>
2017-09-09 22:35:13 -0300

    Primeiro commit

---
+++ src/controle/Controle.java
@@ -0,0 +1,8 @@
+package controle;
+
+public class Controle {
+
+	public String nomeSO() {
+		return System.getProperty("os.name");
+	}
+}
---
+++ src/visao/Visao.java
@@ -0,0 +1,13 @@
+package visao;
+
+import controle.Controle;
+
+public class Visao {
+
+	public static void main(String[] args) {
+		Controle c = new Controle();
+		
+		System.out.println(c.nomeSO());
+	}
+
+}
